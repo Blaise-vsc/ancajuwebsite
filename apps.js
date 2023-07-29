e9731cbc4e00b4ca1b796b142810e84a203e7059
betua blaise bashonga <126053707+Blaise-vsc@users.noreply.github.com>
2023-07-29 09:18:04 +0300

    .

--- apps.js
+++ apps.js
@@ -1,2 +1,19 @@
-const serviceClient = document.querySelector("#serviceClient");
-console.log(serviceClient);
+const welcomeIndex = document.querySelector("#welcomeIndex");
+
+setTimeout(() => {
+  welcomeIndex.innerHTML =
+    "Assiste le jeu de la francophonie en direct chez nous.";
+  welcomeIndex.style.color = "yellow";
+  setTimeout(() => {
+    welcomeIndex.innerHTML = "Ancaju guest house";
+    welcomeIndex.style.color = "bleu";
+    setTimeout(() => {
+      welcomeIndex.innerHTML = "The best hotel in katanga";
+      welcomeIndex.style.color = "red";
+      setTimeout(() => {
+        welcomeIndex.innerHTML = "BIENVENUE A ANCAJU GUEST HOUSE";
+        welcomeIndex.style.color = "white";
+      }, 7000);
+    }, 7000);
+  }, 10000);
+}, 10000);
