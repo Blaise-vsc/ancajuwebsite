4cd6f162385e96f0912c05dca01c9eeb5087826d
betua blaise bashonga <126053707+Blaise-vsc@users.noreply.github.com>
2023-08-14 16:02:49 +0300

    fonts changes in the firspage-scss and in the apps js

--- apps.js
+++ apps.js
@@ -1,19 +1,19 @@
 const welcomeIndex = document.querySelector("#welcomeIndex");
 
-setTimeout(() => {
-  welcomeIndex.innerHTML =
-    "Assiste le jeu de la francophonie en direct chez nous.";
-  welcomeIndex.style.color = "yellow";
-  setTimeout(() => {
-    welcomeIndex.innerHTML = "Ancaju guest house";
-    welcomeIndex.style.color = "bleu";
-    setTimeout(() => {
-      welcomeIndex.innerHTML = "The best hotel in katanga";
-      welcomeIndex.style.color = "red";
-      setTimeout(() => {
-        welcomeIndex.innerHTML = "BIENVENUE A ANCAJU GUEST HOUSE";
-        welcomeIndex.style.color = "white";
-      }, 7000);
-    }, 7000);
-  }, 10000);
-}, 10000);
+// setTimeout(() => {
+//   welcomeIndex.innerHTML =
+//     "Assiste le jeu de la francophonie en direct chez nous.";
+//   welcomeIndex.style.color = "yellow";
+//   setTimeout(() => {
+//     welcomeIndex.innerHTML = "Ancaju guest house";
+//     welcomeIndex.style.color = "bleu";
+//     setTimeout(() => {
+//       welcomeIndex.innerHTML = "The best hotel in katanga";
+//       welcomeIndex.style.color = "red";
+//       setTimeout(() => {
+//         welcomeIndex.innerHTML = "BIENVENUE A ANCAJU GUEST HOUSE";
+//         welcomeIndex.style.color = "white";
+//       }, 7000);
+//     }, 7000);
+//   }, 10000);
+// }, 10000);
---
+++ note.md
@@ -0,0 +1,4 @@
+1 changes:-m "remover the setTimeout" WHERE apps.sj
+2 changes:-m "remover the setTimeout" WHERE firstPage.scss
+-change the font line: 212 =font-family: 'Martel Sans', sans-serif;
+-change the font body: font-family: 'Martel Sans', sans-serif;
