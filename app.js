255e3ac8d198bd2052c46c7a55fcafd1a1972a6d
betua blaise bashonga <126053707+Blaise-vsc@users.noreply.github.com>
2023-08-26 17:05:31 +0300

    change the script

--- app.js
+++ app.js
@@ -2,7 +2,25 @@ const swiper = new Swiper(".swiper", {
   // Optional parameters
   direction: "horizontal",
   loop: true,
+  // cube
+  // effect: "cube",
+  // grabCursor: true,
+  // cubeEffect: {
+  //   shadow: true,
+  //   slideShadows: true,
+  //   shadowOffset: 20,
+  //   shadowScale: 0.94,
+  // },
 
+  slidesPerView: 2,
+  spaceBetween: 7,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  //
   // If we need pagination
   pagination: {
     el: ".swiper-pagination",
--- apps.js
+++ apps.js
@@ -1,19 +1,9 @@
-const welcomeIndex = document.querySelector("#welcomeIndex");
-
-// setTimeout(() => {
-//   welcomeIndex.innerHTML =
-//     "Assiste le jeu de la francophonie en direct chez nous.";
-//   welcomeIndex.style.color = "yellow";
-//   setTimeout(() => {
-//     welcomeIndex.innerHTML = "Ancaju guest house";
-//     welcomeIndex.style.color = "bleu";
-//     setTimeout(() => {
-//       welcomeIndex.innerHTML = "The best hotel in katanga";
-//       welcomeIndex.style.color = "red";
-//       setTimeout(() => {
-//         welcomeIndex.innerHTML = "BIENVENUE A ANCAJU GUEST HOUSE";
-//         welcomeIndex.style.color = "white";
-//       }, 7000);
-//     }, 7000);
-//   }, 10000);
-// }, 10000);
+const serviceClient = document.querySelector("#serviceClient");
+serviceClient.style.display = "none";
+console.log(serviceClient);
+setTimeout(() => {
+  serviceClient.style.display = "flex";
+  setTimeout(() => {
+    serviceClient.style.display = "none";
+  }, 7000);
+}, 3000);
